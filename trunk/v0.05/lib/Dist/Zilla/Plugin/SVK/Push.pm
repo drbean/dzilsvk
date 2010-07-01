2ab87cd3b0ce58e80b14050023662f9a142c7353
drbean <drbean@3645cbeb-586c-df11-b3eb-001a64c68af4>
2010-07-01 04:21:50 +0000

    r5951@sac:  drbean | 2010-07-01 10:52:46 +0800   r5925@sac:  drbean | 2010-06-30 16:06:50 +0800   v0.05

---
+++ trunk/v0.05/lib/Dist/Zilla/Plugin/SVK/Push.pm
@@ -0,0 +1,86 @@
+use 5.008;
+use strict;
+use warnings;
+
+package Dist::Zilla::Plugin::SVK::Push;
+# ABSTRACT: push current branch
+
+use SVK;
+use SVK::XD;
+use SVK::Util qw/find_dotsvk/;
+use File::Basename;
+
+use Moose;
+use MooseX::Has::Sugar;
+use MooseX::Types::Moose qw{ ArrayRef Str };
+
+with 'Dist::Zilla::Role::AfterRelease';
+
+# sub mvp_multivalue_args { qw(push_to) }
+
+# -- attributes
+
+#has push_to => (
+#  is   => 'ro',
+#  isa  => 'ArrayRef[Str]',
+#  lazy => 1,
+#  default => sub { [ qw(origin) ] },
+#);
+
+
+sub after_release {
+    my $self = shift;
+	my $namepart = qr|[^/]*|;
+	my $info = qx "svk info";
+	$info =~ m/^.*\n[^\/]*(\/.*)$/m; my $depotpath = $1;
+	( my $depotname = $depotpath ) =~ s|^/($namepart).*$|$1|;
+	my $project = $self->project || $self->zilla->name;
+	my $project_dir = lc $project;
+	$project_dir =~ s/::/-/g;
+	my $tag_dir = $self->zilla->plugin_named('SVK::Tag')->tag_directory;
+
+	# push everything on remote branch
+	$self->log("pushing to remote");
+	system( 'svk push' );
+	$self->log_debug( "The local changes" );
+	my $switchpath = $depotpath;
+	$switchpath = dirname( $switchpath ) until basename( $switchpath ) eq
+		$project_dir or basename( $switchpath ) eq $depotname;
+	$switchpath .= "/$tag_dir";
+	system( "svk switch $switchpath" );
+	system( 'svk push' );
+	$self->log_debug( "The tags too" );
+}
+
+1;
+__END__
+
+=for Pod::Coverage
+    after_release
+    mvp_multivalue_args
+
+=head1 SYNOPSIS
+
+In your F<dist.ini>:
+
+    [Git::Push]
+    push_to = //mirror/project      ; this is the default
+    push_to = origin HEAD:refs/heads/released ; also push to released branch
+
+
+=head1 DESCRIPTION
+
+Once the release is done, this plugin will push current svk branch to
+remote end, with the associated tags.
+
+
+The plugin accepts the following options:
+
+=over 4
+
+=item * 
+
+push_to - the name of the a remote to push to. The default is F<origin>.
+This may be specified multiple times to push to multiple repositories.
+
+=back
